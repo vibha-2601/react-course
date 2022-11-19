74c3e3a0c89ceb44f8c63c076c89ec00e4962af7
vibha-2601 <vibhanimbalkar2601@gmail.com>
2022-11-19 21:51:12 +0530

    functional component

---
+++ hello-world/src/Components/Greet.js
@@ -0,0 +1,11 @@
+// Functional component
+import React from 'react'
+
+// function Greet(){
+//     return <h1>Hello Robert</h1>
+// }
+
+// ES6 Arrow function
+const Greet=()=><h1>Hello Robert</h1>
+
+export default Greet

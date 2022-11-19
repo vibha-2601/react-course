554d57b97397759ca0f7687a6b593e28a5118fff
vibha-2601 <vibhanimbalkar2601@gmail.com>
2022-11-19 23:10:13 +0530

    Class component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './Components/Greet'
-
+import Welcome from './Components/Welcome'
 function App() {
   return (
     <div className="App">
     <Greet />
+    <Welcome />
     </div>
   );
 }
